3363dcc737ab2b13a82435a89a6d671f931411da
Austin Szema <aszema13@gmail.com>
2023-07-07 16:36:44 -0400

    Added player sprite and added player rotating when hitting walls visually

--- gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
+++ gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
@@ -7,7 +7,7 @@ public class AutoMove : MonoBehaviour
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 5f;
-    
+
     private float moveDirection = 1f;
 
     // Update is called once per frame
@@ -21,6 +21,14 @@ public class AutoMove : MonoBehaviour
         if (collision.gameObject.CompareTag("Wall"))
         {
             moveDirection *= -1f;
+            if(moveDirection > 0f)
+            {
+                transform.rotation = Quaternion.identity;
+            }
+            else
+            {
+                transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+            }
         }
     }
 
