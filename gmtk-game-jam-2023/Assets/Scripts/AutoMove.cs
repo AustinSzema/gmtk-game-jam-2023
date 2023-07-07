ba362dd3f48e6220cac921b3d2371d658b740a5f
Austin Szema <aszema13@gmail.com>
2023-07-07 16:27:04 -0400

    Added Spring, Starting and Ending platform prefabs, and made the player turn around when hitting walls

--- gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
+++ gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
@@ -6,9 +6,8 @@ public class AutoMove : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float moveSpeed = 5f;
     
-    private bool movingRight = true;
     private float moveDirection = 1f;
 
     // Update is called once per frame
@@ -17,11 +16,16 @@ public class AutoMove : MonoBehaviour
         Move();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            moveDirection *= -1f;
+        }
+    }
+
     private void Move()
     {
-        if (movingRight)
-        {
-            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
-        }
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
     }
 }
