d03308ef1cb089634df9126596d8926528e728c5
Austin Szema <aszema13@gmail.com>
2023-07-08 10:12:17 -0400

    Changed some assets to look better

---
+++ gmtk-game-jam-2023/Assets/Spring.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField] private float springPower = 10f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject player = collision.gameObject;
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, springPower);
+        }
+    }
+}
