50d453970b3e1b7484582f9bd2855d34f7b957cf
ramjsandal <ramjsandal@gmail.com>
2023-07-08 15:27:37 -0400

    refixed pushing character around before play

--- gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
+++ gmtk-game-jam-2023/Assets/Scripts/AutoMove.cs
@@ -5,56 +5,73 @@ using UnityEngine;
 
 public class AutoMove : MonoBehaviour
 {
+  [SerializeField] private Rigidbody2D rb;
+  [SerializeField] private float moveSpeed = 5f;
+  [SerializeField] private float triggerJumpDistance = 1f;
+  [SerializeField] private float jumpForce = 10f;
+  [SerializeField] private bool _move = false;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float triggerJumpDistance = 1f;
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private bool _move = false;
+  private float moveDirection = 1f;
+  private Collider2D _collider2D;
+  [SerializeField] private bool isGrounded;
+  RaycastHit2D wallCast;
 
-    private float moveDirection = 1f;
-    [SerializeField] private bool isGrounded;
-    RaycastHit2D wallCast;
+  [SerializeField] private LayerMask whatIsWall;
+  [SerializeField] private LayerMask whatIsGround;
 
-    [SerializeField] private LayerMask whatIsWall;
-    [SerializeField] private LayerMask whatIsGround;
+  [SerializeField] private Transform footPosition;
+  [SerializeField] private Transform frontPosition;
 
-    [SerializeField] private Transform footPosition;
-    [SerializeField] private Transform frontPosition;
-
-    void FixedUpdate()
+  void FixedUpdate()
+  {
+    if (GameController.editing)
     {
-        wallCast = Physics2D.BoxCast(transform.position, new Vector2(triggerJumpDistance, transform.localScale.y), 00f, transform.right, triggerJumpDistance, whatIsWall);
-        RaycastHit2D ray = Physics2D.BoxCast(footPosition.position, new Vector2(transform.localScale.x / 2, 0.1f), 0f, -transform.up, 0.1f, whatIsGround);
-        isGrounded = ray;
-
-        if (wallCast)
-        {
-            Debug.Log("attempting to jump");
-            Jump();
-        }
-
-        /*if (Physics2D.BoxCast(transform.position, new Vector2(0.1f, transform.localScale.y), 00f, transform.right, 0.1f))
-        {
-            FlipDirection();
-        }*/
-
-        Move();
-
+      rb.Sleep();
+      _collider2D.enabled = false;
+    }
+    else
+    {
+      rb.WakeUp();
+      _collider2D.enabled = true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    if (_move)
     {
-        if (Physics2D.BoxCast(frontPosition.position, new Vector2(0.1f, transform.localScale.y), 00f, transform.right, 0.1f, whatIsWall))
-        {
-            FlipDirection();
-        }
-    }
+      wallCast = Physics2D.BoxCast(transform.position, new Vector2(triggerJumpDistance, transform.localScale.y), 00f,
+        transform.right, triggerJumpDistance, whatIsWall);
+      RaycastHit2D ray = Physics2D.BoxCast(footPosition.position, new Vector2(transform.localScale.x / 2, 0.1f), 0f,
+        -transform.up, 0.1f, whatIsGround);
+      isGrounded = ray;
 
-    private void Start()
-    {
-        _move = false;
+      if (wallCast)
+      {
+        Debug.Log("attempting to jump");
+        Jump();
+      }
+
+      /*if (Physics2D.BoxCast(transform.position, new Vector2(0.1f, transform.localScale.y), 00f, transform.right, 0.1f))
+      {
+          FlipDirection();
+      }*/
+
+      Move();
     }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (Physics2D.BoxCast(frontPosition.position, new Vector2(0.1f, transform.localScale.y), 00f, transform.right, 0.1f,
+          whatIsWall))
+    {
+      FlipDirection();
+    }
+  }
+
+  private void Start()
+  {
+    _move = false;
+    _collider2D = GetComponent<Collider2D>();
+  }
 
   private void OnEnable()
   {
@@ -63,37 +80,36 @@ public class AutoMove : MonoBehaviour
     transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
   }
 
-    private void FlipDirection()
+  private void FlipDirection()
+  {
+    moveDirection *= -1f;
+    Debug.Log(moveDirection);
+    if (moveDirection > 0f)
     {
-        
-        moveDirection *= -1f;
-        Debug.Log(moveDirection);
-        if (moveDirection > 0f)
-        {
-            transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
-        }
-        else
-        {
-            transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
-        }
+      transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
     }
-    private void Jump()
+    else
     {
+      transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
+    }
+  }
 
-        if (isGrounded)
-        {
-            Debug.Log("is jump");
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
-    }
-
-    private void Move()
+  private void Jump()
+  {
+    if (isGrounded)
     {
-        if (_move)
-        {
-         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);           
-        }
+      Debug.Log("is jump");
+      rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
+  }
+
+  private void Move()
+  {
+    if (_move)
+    {
+      rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+  }
 
   public void Play()
   {
